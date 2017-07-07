21881c6dc3218ac758ce6b318d0c165f0bec086a
海恩 <1216182374@qq.com>
2017-07-07 16:46:45 +0800

    添加外观模式

---
+++ src/main/java/structuralPatterns/facade/simpleExample/ModuleB.java
@@ -0,0 +1,13 @@
+package main.java.structuralPatterns.facade.simpleExample;
+
+/**
+ * Created by henser on 17-7-7.
+ */
+public class ModuleB {
+
+    //示意方法
+    public void testB(){
+        System.out.println("调用ModuleB中的testB方法");
+    }
+
+}
---
+++ src/main/java/structuralPatterns/facade/simpleExample/ModuleC.java
@@ -0,0 +1,13 @@
+package main.java.structuralPatterns.facade.simpleExample;
+
+/**
+ * Created by henser on 17-7-7.
+ */
+public class ModuleC {
+
+    //示意方法
+    public void testC(){
+        System.out.println("调用ModuleC中的testC方法");
+    }
+
+}
