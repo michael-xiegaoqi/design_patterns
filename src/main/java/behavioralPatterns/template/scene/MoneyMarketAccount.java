89a6341dfac3f494ddb5b83101a18a6f00f8a4d6
海恩 <1216182374@qq.com>
2017-07-09 12:07:26 +0800

    添加模板方法模式

---
+++ src/main/java/behavioralPatterns/template/scene/MoneyMarketAccount.java
@@ -0,0 +1,21 @@
+package main.java.behavioralPatterns.template.scene;
+
+/**
+ * 具体模板角色类
+ * Created by henser on 17-7-9.
+ */
+public class MoneyMarketAccount extends Account {
+
+    @Override
+    protected String doCalculateAccountType() {
+
+        return "Money Market";
+    }
+
+    @Override
+    protected double doCalculateInterestRate() {
+
+        return 0.045;
+    }
+
+}
---
+++ src/main/java/behavioralPatterns/template/simpleExample/AbstractTemplate.java
@@ -0,0 +1,37 @@
+package main.java.behavioralPatterns.template.simpleExample;
+
+/**
+ * Created by henser on 17-7-9.
+ */
+public abstract class AbstractTemplate {
+
+    /**
+     * 模板方法
+     * 定义为final 是为了防止子类进行重载这个模板方法
+     */
+    public final void templateMethod(){
+        //调用基本方法
+        abstractMethod();
+        hookMethod();
+        concreteMethod();
+    }
+
+    /**
+     * 基本方法的声明（由子类实现）
+     */
+    protected abstract void abstractMethod();
+
+    /**
+     * 基本方法(空方法)
+     */
+    protected void hookMethod(){}
+
+    /**
+     * 基本方法（已经实现）
+     */
+    private final void concreteMethod(){
+        //业务相关的代码
+        System.out.println("具体业务相关的代码");
+    }
+
+}
